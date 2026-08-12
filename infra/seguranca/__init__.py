5838c474097305ecf3fcb4f29de42694848f05c1
faabio3131 <134972286+faabio3131@users.noreply.github.com>
2026-08-12 20:38:13 -0300

    V1 Wave0: format infrastructure exports

--- infra/seguranca/__init__.py
+++ infra/seguranca/__init__.py
@@ -1,7 +1,12 @@
 """Persistência e adapters externos da fundação de segurança."""
 
 from .adaptador_sqlalchemy import RepositorioIdentidadesSQLAlchemy
-from .modelos_orm import SecurityBase, UsuarioPapelORM, UsuarioSegurancaORM, UsuarioUnidadeORM
+from .modelos_orm import (
+    SecurityBase,
+    UsuarioPapelORM,
+    UsuarioSegurancaORM,
+    UsuarioUnidadeORM,
+)
 
 __all__ = [
     "RepositorioIdentidadesSQLAlchemy",
